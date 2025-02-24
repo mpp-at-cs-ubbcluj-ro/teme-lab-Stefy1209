a25ffccf74128e779716b40b5e8d4f362158ea93
Stefy1209 <stefangramada09@gmail.com>
2025-02-24 12:37:06 +0200

    Lab assignment 1 schimbare

--- Lab-assignment-1/src/Main.java
+++ Lab-assignment-1/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! The first message from Stefy ^-^");
+        System.out.println("Hello, World! The first message from Stefy ^-^ WOW");
     }
 }
